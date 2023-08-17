5383487822bb9ce6a323f395250351c8e395357a
Petar Cholakov <petar.cholakov30@gmail.com>
2023-08-17 20:35:13 +0100

    Create largestNumber.js

---
+++ Syntax, Functions and Statements - Lab/largestNumber.js
@@ -0,0 +1,6 @@
+function largestNum(numOne, numTwo, numThree) {
+    console.log(`The largest number is ${Math.max(numOne, numTwo, numThree)}.`);
+}
+
+largestNum(5, -3, 16);
+largestNum(-3, -5, -22.5);
