f4e98b20d1dcb646e19551fb8afc8ae2b5a600de
Petar Cholakov <petar.cholakov30@gmail.com>
2023-08-18 06:59:01 +0100

    Create mathOperations.js

---
+++ Syntax, Functions and Statements - Lab/mathOperations.js
@@ -0,0 +1,29 @@
+function mathOperations(numOne, numTwo, operator) {
+
+    let result;
+
+    switch(operator) {
+        case '+':
+            result = numOne + numTwo;
+            break;
+        case '-':
+            result = numOne - numTwo;
+            break;
+        case '*':
+            result = numOne * numTwo;
+            break;
+        case '/':
+            result = numOne / numTwo;
+            break;
+        case '%':
+            result = numOne % numTwo;
+            break;
+        case '**':
+            result = Math.pow(numOne, numTwo);
+            break;
+    }
+    console.log(result);
+}
+
+mathOperations(5, 6, '+');
+mathOperations(3, 5.5, '*');
