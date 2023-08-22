c58c8d9067bc8f13e957235fc535501a98100306
Petar Cholakov <petar.cholakov30@gmail.com>
2023-08-22 17:07:43 +0100

    Create printAnArrayWithAGivenDelimiter.js

---
+++ Arrays and Nested Arrays - Exercise/printAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,21 @@
+function printArrWithDelimiter(arr, delimiter) {
+
+    console.log(arr.join(delimiter));
+}
+
+printArrWithDelimiter([
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-');
+
+printArrWithDelimiter([
+    'How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_');
