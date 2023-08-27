b6a75e772e2d1d569d7158358604483f7252c5c7
Petar Cholakov <petar.cholakov30@gmail.com>
2023-08-27 16:31:51 +0100

    Create cityRecord.js

---
+++ Objects and Composition - Lab/cityRecord.js
@@ -0,0 +1,14 @@
+function cityRecord(name, population, treasury) {
+    
+    let myObj = {
+        name,
+        population,
+        treasury,
+    }
+    return myObj; 
+}
+
+cityRecord(
+    'Tortuga',
+    7000,
+    15000);
