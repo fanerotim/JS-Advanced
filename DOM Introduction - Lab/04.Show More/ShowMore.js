50d7c59fe7ebe1b61c14178f14a1034c7818746b
Petar Cholakov <petar.cholakov30@gmail.com>
2023-09-02 19:50:48 +0100

    fourth DOM exercise

---
+++ DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    
+    let readMoreElement = document.getElementById('more');
+    let hiddenElement = document.getElementById('text');
+    hiddenElement.style.display = 'inline'
+    readMoreElement.style.display = 'none';
+}
