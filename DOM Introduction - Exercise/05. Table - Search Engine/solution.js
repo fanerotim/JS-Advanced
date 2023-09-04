361f14c8494da0b32ed6bc5e60fc30f0429978aa
Petar Cholakov <petar.cholakov30@gmail.com>
2023-09-04 09:40:33 +0100

    Update solution.js

--- DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -5,7 +5,6 @@ function solve() {
       
       let cells = document.querySelectorAll('tbody tr');
       let input = document.getElementById('searchField').value;
-      console.log(cells);
 
       for (let cell of cells) {
          if (cell.textContent.includes(input)) {
@@ -13,6 +12,7 @@ function solve() {
          } else {
             cell.classList.remove('select');
          }
+         document.getElementById('searchField').value = '';
       }
    }
 }
