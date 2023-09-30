c9cd19f83c783e6ab821c75893aba1f8edda0f40
Petar Cholakov <petar.cholakov30@gmail.com>
2023-09-30 13:43:17 +0100

    exercise

---
+++ Unit Testing and Error Handling - Exercise/test/evenOrOdd.test.js
@@ -0,0 +1,29 @@
+const isOddOrEven = require('../evenOrOdd');
+const solve = require('../evenOrOdd');
+const expect = require('chai').expect;
+
+describe('even or odd', () => {
+    it('checks if result is undefined', () => {
+        let input = 1;
+        let expectedResult = undefined;
+        let actualResult = isOddOrEven(input);
+
+        expect(actualResult).to.equal(expectedResult);
+    });
+
+    it('checks is length is even', () => {
+        let input = 'autumn';
+        let expectedResult = 'even';
+        let actualResult = isOddOrEven(input);
+
+        expect(actualResult).to.equal(expectedResult);
+    })
+
+    it('checks if length is odd', () => {
+        let input = 'sunny';
+        let expectedResult = 'odd';
+        let actualResult = isOddOrEven(input);
+
+        expect(actualResult).to.equal(expectedResult);
+    })
+})
