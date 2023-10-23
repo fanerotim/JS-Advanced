a8f1c0eb64e6c47ca67765ddf82af61c7a440749
Petar Cholakov <petar.cholakov30@gmail.com>
2023-10-23 17:30:11 +0100

    Class Prototypes exercise

---
+++ Prototypes and Inheritance - Lab/extendPrototype.js
@@ -0,0 +1,7 @@
+function solve(input) {
+
+    input.prototype.species = 'Human';
+    input.prototype.toSpeciesString = function() {
+        return `I am a ${this.species}. ${this.toString()}`
+    }
+}
